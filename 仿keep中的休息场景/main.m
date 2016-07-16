189b1d3dcf0a76cb8cc33068621ab9306575a6cc
残月 <632342527@qq.com>
2016-07-16 15:14:55 +0800

    Initial Commit

---
+++ 仿keep中的休息场景/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  仿keep中的休息场景
+//
+//  Created by 王奥东 on 16/7/16.
+//  Copyright © 2016年 王奥东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
